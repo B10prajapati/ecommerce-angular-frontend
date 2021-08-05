bdcf511e557a8512307efca63283d2b1c2593050
Prajapati, Sahas <b10prajapati@gmail.com>
2021-08-05 18:03:53 +0545

    environment var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
+import 'dotenv/path';
 export const environment = {
   production: true,
 
-  baseURL: 'https://ecommerce-nestjs-backend.herokuapp.com/api',
+  baseURL: process.env.baseURL,
+  cookieExpiresIn: process.env.cookieExpiresIn,
+  cookiePath: process.env.cookiePath,
+  cookieXsrf: process.env.cookieXsrf,
+  headerXsrf: process.env.headerXsrf,
 };
